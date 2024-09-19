51308414f2e345043caad9cf0487a3a618ccea47
thian10 <DianM.job@gmail.com>
2024-09-19 12:32:34 +0700

    add contact

--- main.js
+++ main.js
@@ -14,3 +14,24 @@ function toggleDetail(e) {
     $(detail).slideToggle()
 
 }
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+
+    if(!$(email).val()) {
+        alert("Email Is required")
+    } else if (!$(subject).val()) {
+        alert("subject is required")
+    } else if (!$(message).val()) {
+        alert("message is requires") 
+    } else {
+        alert("form Submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
+}
